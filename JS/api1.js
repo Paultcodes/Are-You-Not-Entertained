fc263259e3ec83eff24748d359e29a22abeab2d5
Paultcodes <paultayloremail1995@gmail.com>
2022-10-25 20:56:54 -0500

    Added JS functionality for rendering movie results

--- JS/api1.js
+++ JS/api1.js
@@ -1,37 +1,36 @@
-//OMDb API
+var button = document.getElementById("movie-btn");
 
+button.addEventListener("click", populateMovie);
 
-// console.log(completeURL);
+function populateMovie() {
+  var userInput = document.querySelector(".user-input");
+  var userChoice = userInput.value;
+  userInput.value = "";
 
-addEventListener("click", populateMovie)
+  var completeURL = "http://www.omdbapi.com/?apikey=98b986e7&t=" + userChoice;
 
-function populateMovie(){
-    var baseURL = "https://www.omdbapi.com/"
-    var apiKeyString = "&apikey=90db5d26&"
-    var movieName = 'batman'
-    var queryString 
-    var queryString = `?t=${movieName}`
-
-    var button = document.getElementById("movie-btn");
-
-    var completeURL = baseURL + queryString + apiKeyString;
-    // var userChoice = userInput.value
-    
-    fetch(completeURL)
-    .then(function(response){
-        return response.json()
+  fetch(completeURL)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayResults(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
     })
-    .then(function(data){
-        console.log(data);
-        displayResults(data);
-    })
+    .catch(function (error) {
+      alert("Unable to connect to OMDB Api");
+    });
 }
 
-populateMovie();
-
-function displayResults(jsonFile){
-    console.log(jsonFile.Title)
-    console.log(jsonFile.Rated)
-    console.log(jsonFile.Runtime)
-    console.log(jsonFile.imdbRating)
+function displayResults(jsonFile) {
+  var title = document.querySelector(".title");
+  title.textContent = "Title: " + jsonFile.Title;
+  var rated = document.querySelector(".rated");
+  rated.textContent = "Rated: " + jsonFile.Rated;
+  var runTime = document.querySelector(".runtime");
+  runTime.textContent = "Runtime: " + jsonFile.Runtime;
+  var rating = document.querySelector(".rating");
+  rating.textContent = "Rating: " + jsonFile.imdbRating;
 }
