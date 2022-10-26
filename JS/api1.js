9a1ea95efacd26e95822189d54c7256a5fbb0e62
Kerry <kerryhokanson@gmail.com>
2022-10-25 20:17:21 -0500

    movie api functionality added

--- JS/api1.js
+++ JS/api1.js
@@ -1,13 +1,37 @@
 //OMDb API
 
-var baseURL = "http://www.omdbapi.com/"
-var apiKeyString = "&apikey=90db5d26&"
-var movieName = 'batman'
-var queryString 
-var queryString = `?t=${movieName}`
 
-var completeURL = baseURL + queryString + apiKeyString;
-console.log(completeURL);
+// console.log(completeURL);
 
+addEventListener("click", populateMovie)
 
+function populateMovie(){
+    var baseURL = "https://www.omdbapi.com/"
+    var apiKeyString = "&apikey=90db5d26&"
+    var movieName = 'batman'
+    var queryString 
+    var queryString = `?t=${movieName}`
 
+    var button = document.getElementById("movie-btn");
+
+    var completeURL = baseURL + queryString + apiKeyString;
+    // var userChoice = userInput.value
+    
+    fetch(completeURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+        displayResults(data);
+    })
+}
+
+populateMovie();
+
+function displayResults(jsonFile){
+    console.log(jsonFile.Title)
+    console.log(jsonFile.Rated)
+    console.log(jsonFile.Runtime)
+    console.log(jsonFile.imdbRating)
+}
