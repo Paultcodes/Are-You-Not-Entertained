b6973cde4b7822e3ab27e31f7fa16c348ddb129e
Paultcodes <paultayloremail1995@gmail.com>
2022-11-01 18:20:44 -0500

    Cleaned up code, added some finishing touches to JS and CSS

--- JS/api1.js
+++ JS/api1.js
@@ -1,4 +1,6 @@
 var button = document.getElementById("movie-btn");
+var results = document.querySelector(".results-flex");
+var errorMessage = document.querySelector(".error-msg");
 
 button.addEventListener("click", populateMovie);
 
@@ -15,38 +17,37 @@ function populateMovie() {
           displayResults(data);
         });
       } else {
-        alert("Error: " + response.statusText);
+        errorMessage.textContent = "Error: " + response.statusText;
       }
     })
     .catch(function (error) {
-      alert("Unable to connect to OMDB Api");
+      errorMessage.textContent = "⚠️Unable to connect to OMDB Api⚠️";
     });
 }
 
 function displayResults(jsonFile) {
   var title = document.querySelector(".title");
   title.textContent = "Title: " + jsonFile.Title;
+  if (!jsonFile.Title) {
+    title.textContent = "Please Search For A Valid Movie";
+    return;
+  }
   var rated = document.querySelector(".rated");
   rated.textContent = "Rated: " + jsonFile.Rated;
   var ratingLetter = jsonFile.Rated;
-  if(ratingLetter === 'G'){
-    rated.style.color='green';
-  }
-  else if(ratingLetter ==='PG'){
-    rated.style.color='yellow';
-  }
-  else if(ratingLetter ==='PG-13'){
-    rated.style.color='orange';
-  }
-  else if(ratingLetter ==='R'){
-    rated.style.color='red';
-  }
-  else{
-    rated.style.color='gray';
+  if (ratingLetter === "G") {
+    rated.style.color = "green";
+  } else if (ratingLetter === "PG") {
+    rated.style.color = "yellow";
+  } else if (ratingLetter === "PG-13") {
+    rated.style.color = "orange";
+  } else if (ratingLetter === "R") {
+    rated.style.color = "red";
+  } else {
+    rated.style.color = "gray";
   }
   var runTime = document.querySelector(".runtime");
   runTime.textContent = "Runtime: " + jsonFile.Runtime;
   var rating = document.querySelector(".rating");
   rating.textContent = "Rating: " + jsonFile.imdbRating;
 }
-
